1d2d767387b508dc61b3cd97a149d4c2225e134b
AtnanAriAnderson <atnanarianderson@gmail.com>
2024-06-06 00:04:31 +0700

    fixing templates

--- view/settings.py
+++ view/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-                 os.path.join(BASE_DIR, 'apps/templates')],
+                 os.path.join(BASE_DIR, 'apps/templates/apps')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
